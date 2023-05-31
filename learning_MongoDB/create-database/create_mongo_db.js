b112e991b4a2b3108edbca8c520417bbc95a1e49
Aw3som3ss <jabwunza@gmail.com>
2023-05-31 20:17:06 +0300

    ceating a database

---
+++ learning_MongoDB/create-database/create_mongo_db.js
@@ -0,0 +1,34 @@
+/*
+To create a database in mongoDB:
+start by creating a MongoClient object
+Then specify a connection URL with the correct IP address,
+ and name of database to be created.
+*/
+
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'myProject';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('documents');
+
+  // the following code examples can be pasted here...
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
